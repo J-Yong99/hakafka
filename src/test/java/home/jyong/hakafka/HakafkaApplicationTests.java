5fadc39991163272542d3be7fee9ac0414a03ae0
Kim Jae Yong <77261304+J-Yong99@users.noreply.github.com>
2025-01-16 13:30:40 +0900

    Add files via upload

---
+++ src/test/java/home/jyong/hakafka/HakafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package home.jyong.hakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakafkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
