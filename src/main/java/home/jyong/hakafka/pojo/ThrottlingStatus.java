ecdb570dfce3d2c297777dcfd3293d5e2b90621c
DESKTOP-0354OJM\ETNERS <alexj99@etners.com>
2025-01-22 09:16:12 +0900

    :spark: feat: throttlingStatus 추가, mysql 로컬데이터 마운트

--- src/main/java/home/jyong/hakafka/pojo/ThrottlingStatus.java
+++ src/main/java/home/jyong/hakafka/pojo/ThrottlingStatus.java
@@ -5,15 +5,30 @@ import lombok.Data;
 @Data
 public class ThrottlingStatus {
 
+  // 전압 부족 상태(현재 전압 부족 발생).
   private boolean underVoltage;
+
+  // ARM CPU 주파수 제한(과열로 인해 성능 제한).
   private boolean frequencyCapped;
+
+  // 현재 성능 제한(전압 부족 또는 과열).
   private boolean currentlyThrottled;
+
+  // 과열
   private boolean temperatureLimit;
 
+
   private boolean underVoltageOccurred;
+
+
   private boolean frequencyCappedOccurred;
+
+
   private boolean throttlingOccurred;
+
+
   private boolean temperatureLimitOccurred;
+
 
   @Override
   public String toString() {
